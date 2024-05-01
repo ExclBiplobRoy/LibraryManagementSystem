d41edef0a463b85ecb29b4ea5d129378f4e7868c
roybi <roybi@DESKTOP-8KB21A5>
2024-05-01 23:17:07 +0600

    Update

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,7 +1,5 @@
 ﻿using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Reflection.Emit;
 
 namespace Data
 {
@@ -11,29 +9,9 @@ namespace Data
         {
         }
 
-        // Define DbSet properties for your entities
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Member> Members { get; set; }
         public DbSet<BorrowedBook> BorrowedBooks { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // Configure entity relationships, constraints, etc.
-            modelBuilder.Entity<Book>()
-                .HasOne(b => b.Author)
-                .WithMany(a => a.Book)
-                .HasForeignKey(b => b.AuthorID);
-
-            modelBuilder.Entity<BorrowedBook>()
-                .HasOne(bb => bb.Member)
-                .WithMany(m => m.BorrowedBook)
-                .HasForeignKey(bb => bb.MemberID);
-
-            modelBuilder.Entity<BorrowedBook>()
-                .HasOne(bb => bb.Book)
-                .WithMany()
-                .HasForeignKey(bb => bb.BookID);
-        }
     }
 }

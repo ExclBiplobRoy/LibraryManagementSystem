3fd7e1bca5cfbf4e21a52972d09942add29182a3
roybi <roybi@DESKTOP-8KB21A5>
2024-05-03 17:50:46 +0600

    Admin WPF operation Done

--- WPFLibraryManagementSystem/Services/AdminServices.cs
+++ WPFLibraryManagementSystem/Services/AdminServices.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Utilities;
 
@@ -30,5 +31,55 @@ namespace WPFLibraryManagementSystem.Services
             var content = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<List<Admin>>(content);
         }
+
+        public async Task CreateAdminAsync(Domain.Entities.Admin admin)
+        {
+            try
+            {
+                string RequestRoute = $"{BaseUrl}{RouteConstants.BaseRoute}/{RouteConstants.CreateAdmin}";
+
+                var json = JsonConvert.SerializeObject(admin);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await _client.PostAsync(RequestRoute, content);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while creating the admin: {ex.Message}");
+            }
+        }
+
+        public async Task UpdateAdminAsync(Domain.Entities.Admin admin)
+        {
+            try
+            {
+                string RequestRoute = $"{BaseUrl}{RouteConstants.BaseRoute}/{RouteConstants.UpdateAdmin.Replace("{key}", admin.AdminID.ToString())}";
+                string jsonAdmin = JsonConvert.SerializeObject(admin);
+                HttpContent content = new StringContent(jsonAdmin, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await _client.PutAsync(RequestRoute, content);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while updating the admin: {ex.Message}");
+            }
+        }
+
+        public async Task DeleteAdminAsync(int adminId)
+        {
+            try
+            {
+                string RequestRoute = $"{BaseUrl}{RouteConstants.BaseRoute}/{RouteConstants.DeleteAdmin.Replace("{key}", adminId.ToString())}?id={adminId}";
+
+                HttpResponseMessage response = await _client.DeleteAsync(RequestRoute);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
     }
 }
--- WPFLibraryManagementSystem/View/AdminTest.xaml.cs
+++ WPFLibraryManagementSystem/View/AdminTest.xaml.cs
@@ -1,22 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 using WPFLibraryManagementSystem.Services;
 
 namespace WPFLibraryManagementSystem.View
 {
-    /// <summary>
-    /// Interaction logic for AdminTest.xaml
-    /// </summary>
     public partial class AdminTest : Window
     {
         private readonly AdminService _adminService;
+
+        public Domain.Entities.Admin NewAdmin { get; set; }
+
+        public string FullName;
 
         public AdminTest()
         {
             InitializeComponent();
             _adminService = new AdminService();
+            NewAdmin = new Domain.Entities.Admin();
             LoadAdmins();
+
+            AdminList.SelectionChanged += AdminList_SelectionChanged;
+        }
+
+        private void AdminList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AdminList.SelectedItem != null)
+            {
+                var selectedAdmin = (Domain.Entities.Admin)AdminList.SelectedItem;
+
+                FirstNameTextBox.Text = selectedAdmin.FirstName;
+                LastNameTextBox.Text = selectedAdmin.SurName;
+                EmailTextBox.Text = selectedAdmin.Email;
+                PasswordBox.Password = selectedAdmin.Password;
+                LevelTextBox.Text = selectedAdmin.Level.ToString();
+
+                btnCreateAdmin.Content = "Update";
+            }
         }
 
         private async void LoadAdmins()
@@ -24,16 +45,90 @@ namespace WPFLibraryManagementSystem.View
             try
             {
                 List<Domain.Entities.Admin> admins = await _adminService.GetAdminsAsync();
-                foreach (var admin in admins)
-                {
-                    admin.FirstName += " " + admin.SurName;
-                }
                 AdminList.ItemsSource = admins;
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"An error occurred while loading admins: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+        }
+
+        private async void CreateAdminButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string firstName = FirstNameTextBox.Text;
+                string lastName = LastNameTextBox.Text;
+                string email = EmailTextBox.Text;
+                string password = PasswordBox.Password;
+                int level = Convert.ToInt32(LevelTextBox.Text);
+
+                if (btnCreateAdmin.Content.ToString() == "Create")
+                {
+                    Domain.Entities.Admin newAdmin = new Domain.Entities.Admin
+                    {
+                        FirstName = firstName,
+                        SurName = lastName,
+                        Email = email,
+                        Password = password,
+                        Level = level
+                    };
+                    await _adminService.CreateAdminAsync(newAdmin);
+                }
+                else if (btnCreateAdmin.Content.ToString() == "Update")
+                {
+                    var selectedAdmin = (Domain.Entities.Admin)AdminList.SelectedItem;
+                    selectedAdmin.FirstName = firstName;
+                    selectedAdmin.SurName = lastName;
+                    selectedAdmin.Email = email;
+                    selectedAdmin.Password = password;
+                    selectedAdmin.Level = level;
+
+                    await _adminService.UpdateAdminAsync(selectedAdmin);
+                }
+
+                LoadAdmins();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while processing the operation: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private async void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Domain.Entities.Admin selectedAdmin = AdminList.SelectedItem as Domain.Entities.Admin;
+
+                if (selectedAdmin == null)
+                {
+                    MessageBox.Show("Please select an admin to delete.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                await _adminService.DeleteAdminAsync(selectedAdmin.AdminID);
+
+                LoadAdmins();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while deleting the admin: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        private void ClearFields()
+        {
+            FirstNameTextBox.Clear();
+            LastNameTextBox.Clear();
+            EmailTextBox.Clear();
+            PasswordBox.Clear();
+            LevelTextBox.Clear();
+
+            btnCreateAdmin.Content = "Create";
         }
     }
 }
